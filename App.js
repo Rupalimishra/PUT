afadc7239540d4443dd7e8d0e6da740b736c9e5d
Rupalimishra <rupalimishra389@gmail.com>
2020-07-06 17:43:26 +0530

    PUT

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { Text } from 'react-native';
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {user: []};
+	}
+	
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/posts/1', {
+			method: 'PUT',
+			body: JSON.stringify({
+				id: 1,
+				title: 'foo',
+				body: 'bar',
+				userId: 1
+			}),
+			headers: {
+			  "Content-type": "application/json; charset=UTF-8"
+			}
+		}).then(response => {
+						return response.json()
+		}).then(json => {
+			console.log(json)
+			this.setState({
+				user:json
+			});
+		})
+	}
+	render() {                            
+		return (
+			<Text>
+				<Text>Resource updated in the server as shown below</Text>
+				<Text>Id : {this.state.user.id}</Text>
+				<Text>Title : {this.state.user.title}</Text>
+				<Text>Body : {this.state.user.body}</Text>
+				<Text>UserId : {this.state.user.userId}</Text>
+			</Text>
+		)
+	}
+   }
+export default App;
